104fa5a53ab6e2a25d8e88fb2b3bab6690466ac9
vipisanan <vipisanan1@gmail.com>
2019-09-06 16:43:17 +0530

    form and two way binding

--- src/hello/Hello.js
+++ src/hello/Hello.js
@@ -7,51 +7,64 @@ class Hello extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            data: 0
+            data: 'Initial data...',
+            date: 'vipi'
         }
-        this.setNewNumber = this.setNewNumber.bind(this)
-
+        this.updateState = this.updateState.bind(this);
     };
 
-    setNewNumber() {
-        this.setState({data: this.state.data + 1})
+    updateState(e) {
+        this.setState({data: e.target.value});
     }
 
     render() {
         console.log('main render');
-
         return (
             <div>
-                <button onClick={this.setNewNumber}>INCREMENT</button>
-                <Content myNumber={this.state.data}></Content>
+                <div>
+                    <input type="text" value={this.state.data}
+                           onChange={this.updateState}/>
+                    <h4>{this.state.data} +  {this.state.date}</h4>
+                </div>
             </div>
         );
     }
 
 }
 
+
+
+
+//it's for lifecycle
 class Content extends React.Component {
     componentWillMount() {
         console.log('Component WILL MOUNT!')
     }
+
     componentDidMount() {
         console.log('Component DID MOUNT!')
     }
+
     componentWillReceiveProps(newProps) {
         console.log('Component WILL RECIEVE PROPS!')
     }
+
     shouldComponentUpdate(newProps, newState) {
         return true;
     }
+
     componentWillUpdate(nextProps, nextState) {
         console.log('Component WILL UPDATE!');
     }
+
     componentDidUpdate(prevProps, prevState) {
         console.log('Component DID UPDATE!')
     }
+
     componentWillUnmount() {
         console.log('Component WILL UNMOUNT!')
     }
+
     render() {
         console.log('sub render');
 
@@ -62,6 +75,5 @@ class Content extends React.Component {
         );
     }
 }
-
 
 export default Hello;
