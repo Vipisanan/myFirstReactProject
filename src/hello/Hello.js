43aba27e6e79617ded1f1aabd834cfaa01b292c8
vipisanan <vipisanan1@gmail.com>
2019-09-06 17:42:28 +0530

    dynamic component loop

--- src/hello/Hello.js
+++ src/hello/Hello.js
@@ -6,43 +6,53 @@ import './hello.css';
 class Hello extends React.Component {
     constructor(props) {
         super(props);
+
         this.state = {
-            data: 'Initial data...',
-            date: 'vipi'
+            data: [
+                {
+                    component: 'First...',
+                    id: 1
+                },
+                {
+                    component: 'Second...',
+                    id: 2
+                },
+                {
+                    component: 'Third...',
+                    id: 3
+                }
+            ]
         }
-        this.updateState = this.updateState.bind(this);
+
     };
 
-    updateState(e) {
-        console.log("updateState")
-
-        this.setState({data: e.target.value});
-    }
 
     render() {
         console.log('main render');
         return (
             <div>
-                <Content myDataProp={this.state.data}
-                         updateStateProp={this.updateState}>
-                </Content>
+                <div>
+                    {this.state.data.map((dynamicComponent, i) =>
+                        <Content key={i} componentData={dynamicComponent}/>)}
+                    {this.state.data.map((dyData , i) =>
+                        <h1>{dyData.component}</h1>
+                    )}
+                </div>
             </div>
         );
     }
-
 }
-
 
 class Content extends React.Component {
     render() {
         return (
             <div>
-                <input type="text" value={this.props.myDataProp}
-                       onChange={this.props.updateStateProp}/>
-                <h3>{this.props.myDataProp}</h3>
+                <div>{this.props.componentData.component}</div>
+                <div>{this.props.componentData.id}</div>
             </div>
         );
     }
 }
+
 
 export default Hello;
